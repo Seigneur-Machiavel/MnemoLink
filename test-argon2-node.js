bfe4ba3a9c2a876daff9c4e80eaee724c16b648e
Seigneur-Machiavel <guibi50ae@hotmail.fr>
2024-07-10 12:19:35 +0200

    fix

--- test-argon2-node.js
+++ test-argon2-node.js
@@ -1,9 +1,6 @@
 const argon2 = require("argon2");
-//const argon2 = require("./dist/argon2-bundled.min.js");
-//const argon2 = require("./argon2");
 
 async function test() {
     const toto = await argon2.hash('password', { salt: Buffer.from("saltalittlebitlonger") });
-    //const toto = await argon2.hash({ pass: 'password', salt: 'somesalt' });
     console.log(toto.hash);
 }; test();
